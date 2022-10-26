160ed675d0e9fd78c39bcfbaa3a10c779bf674fd
Ventsislav Tanev <ventsislav.tanev@smartsheet.com>
2022-10-26 15:50:12 +0100

    fix(build): removes javax flag from launch script as javax is no longer part of JDK as of v 11

--- sdk_tests_package/launch.sh
+++ sdk_tests_package/launch.sh
@@ -6,7 +6,5 @@ EXTENSION='ApiScenarioTransformer'
 
 PORT=8082
 
-export JDK_JAVA_OPTIONS='--add-modules java.xml.bind'
-
 # launch wiremock
 java  -cp "libs/*" $MAIN_CLASS --extensions=$EXTENSION --port=$PORT
